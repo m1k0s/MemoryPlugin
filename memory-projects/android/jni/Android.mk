aae6a44df3eb07557c520e0806549b3c18056b02
Michael Steliaros <mike@steliaros.com>
2018-03-16 20:47:53 +0000

    Need to add the logging lib

--- memory-projects/android/jni/Android.mk
+++ memory-projects/android/jni/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES += .
 LOCAL_CFLAGS += -fvisibility=hidden
 LOCAL_ARM_MODE := arm
-LOCAL_MODULE    := memory
+LOCAL_MODULE := memory
 LOCAL_SRC_FILES := memory.cpp
+LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
