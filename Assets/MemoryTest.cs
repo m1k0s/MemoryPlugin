5273e75ce80e0217a49f36f74bd9f6d5f0c66630
Michael Steliaros <mike@steliaros.com>
2018-03-16 23:57:55 +0000

    Dont try to be clever - just report all 4 numbers

--- Assets/MemoryTest.cs
+++ Assets/MemoryTest.cs
@@ -8,8 +8,6 @@ public class MemoryTest : MonoBehaviour
     public Text text;
 
     private StringBuilder _builder = new StringBuilder();
-    private long _lastMemoryKB;
-    private const long BYTE_TO_KILOBYTE = 1000;
     private const long BYTE_TO_MEGABYTE = 1000 * 1000;
 
     private void OnEnable()
@@ -19,26 +17,24 @@ public class MemoryTest : MonoBehaviour
             enabled = false;
             return;
         }
-
-        _lastMemoryKB = -1;
     }
 
     private void Update()
     {
+        _builder.Length = 0;
+
         long memory = Memory.processResidentUsed;
-        long memoryKB = (memory + BYTE_TO_KILOBYTE - 1) / BYTE_TO_KILOBYTE;
-        if (_lastMemoryKB != memoryKB)
-        {
-            _lastMemoryKB = memoryKB;
-            _builder.Length = 0;
-            _builder.Append("Process Resident: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
-            memory = Memory.processVirtualUsed;
-            _builder.Append("Process Virtual: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
-            memory = Memory.systemFree;
-            _builder.Append("System Free: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
-            memory = Memory.systemTotal;
-            _builder.Append("System Total: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
-            text.text = _builder.ToString();
-        }
+        _builder.Append("Process Resident: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+
+        memory = Memory.processVirtualUsed;
+        _builder.Append("Process Virtual: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+
+        memory = Memory.systemFree;
+        _builder.Append("System Free: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+
+        memory = Memory.systemTotal;
+        _builder.Append("System Total: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+
+        text.text = _builder.ToString();
     }
 }
