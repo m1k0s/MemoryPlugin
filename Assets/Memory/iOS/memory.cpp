a11b2b4aa4962dfe4a611b9b8d2b83d87fcfd821
Michael Steliaros <mike@steliaros.com>
2018-03-16 20:45:15 +0000

    Added SystemTotalMemory & implemented that & SystemFreeMemory for Android too

--- Assets/Memory/iOS/memory.cpp
+++ Assets/Memory/iOS/memory.cpp
@@ -1,7 +1,11 @@
 #include <stdint.h>
 
-#ifdef __MACH__
+#if defined(__MACH__)
 #include <mach/mach.h>
+#elif defined(__ANDROID__)
+#include <fcntl.h>
+#include <stdlib.h>
+#include <android/log.h>
 #endif
 
 #if defined(_WIN32) && !defined(__SCITECH_SNAP__)
@@ -22,7 +26,7 @@
 extern "C"
 PLUGIN_APICALL int64_t PLUGIN_APIENTRY ProcessResidentMemory()
 {
-#ifdef __MACH__
+#if defined(__MACH__)
     mach_port_t task = mach_task_self();
     struct task_basic_info info;
     mach_msg_type_number_t size = sizeof(info);
@@ -37,7 +41,7 @@ PLUGIN_APICALL int64_t PLUGIN_APIENTRY ProcessResidentMemory()
 extern "C"
 PLUGIN_APICALL int64_t PLUGIN_APIENTRY ProcessVirtualMemory()
 {
-#ifdef __MACH__
+#if defined(__MACH__)
     mach_port_t task = mach_task_self();
     struct task_basic_info info;
     mach_msg_type_number_t size = sizeof(info);
@@ -49,10 +53,67 @@ PLUGIN_APICALL int64_t PLUGIN_APIENTRY ProcessVirtualMemory()
     return 0;
 }
 
+#if defined(__ANDROID__)
+static int64_t getFreeMemoryImpl(const char* const from, const char* const sums[], const size_t sumsLen[], size_t num)
+{
+    int fd = open("/proc/meminfo", O_RDONLY | O_CLOEXEC);
+    if(fd < 0)
+    {
+        __android_log_print(ANDROID_LOG_DEBUG, from, "Unable to open /proc/meminfo\n");
+        return -1;
+    }
+
+    char buffer[256];
+    const int len = read(fd, buffer, sizeof(buffer) - 1);
+    close(fd);
+    if(len < 0)
+    {
+        __android_log_print(ANDROID_LOG_DEBUG, from, "Unable to read /proc/meminfo\n");
+        return -1;
+    }
+
+    // 0 terminate the buffer we just read.
+    buffer[len] = 0;
+
+    int numFound = 0;
+    int64_t mem = 0;
+    for(char* p = buffer; *p && numFound < num; ++p)
+    {
+        for(int i = 0; sums[i]; ++i)
+        {
+            if (strncmp(p, sums[i], sumsLen[i]) == 0)
+            {
+                p += sumsLen[i];
+
+                while(*p == ' ') ++p;
+
+                char* num = p;
+
+                while (*p >= '0' && *p <= '9') ++p;
+
+                if(*p != 0)
+                {
+                    *p++ = 0;
+
+                    if(*p == 0) --p;
+                }
+
+                mem += atoll(num) * 1024;
+                numFound++;
+
+                break;
+            }
+        }
+    }
+
+    return numFound > 0 ? mem : -1;
+}
+#endif
+
 extern "C"
 PLUGIN_APICALL int64_t PLUGIN_APIENTRY SystemFreeMemory()
 {
-#ifdef __MACH__
+#if defined(__MACH__)
     mach_port_t host = mach_host_self();
     mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
     vm_size_t pagesize;
@@ -64,6 +125,34 @@ PLUGIN_APICALL int64_t PLUGIN_APIENTRY SystemFreeMemory()
             return static_cast<int64_t>(vm_stat.free_count * pagesize);
         }
     }
+#elif defined(__ANDROID__)
+    static const char* const sums[] = { "MemFree:", "Cached:", NULL };
+    static const size_t sumsLen[] = { strlen("MemFree:"), strlen("Cached:"), 0 };
+    return getFreeMemoryImpl("SystemFreeMemory", sums, sumsLen, 2);
+#endif
+    return 0;
+}
+
+extern "C"
+PLUGIN_APICALL int64_t PLUGIN_APIENTRY SystemTotalMemory()
+{
+#if defined(__MACH__)
+    mach_port_t host = mach_host_self();
+    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+    vm_size_t pagesize;
+    vm_statistics_data_t vm_stat;
+    if(KERN_SUCCESS == host_page_size(host, &pagesize))
+    {
+        if(KERN_SUCCESS == host_statistics(host, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size))
+        {
+			natural_t used_count = vm_stat.active_count + vm_stat.inactive_count + vm_stat.wire_count;
+            return static_cast<int64_t>((vm_stat.free_count + used_count) * pagesize);
+        }
+    }
+#elif defined(__ANDROID__)
+    static const char* const sums[] = { "MemTotal:", NULL };
+    static const size_t sumsLen[] = { strlen("MemTotal:"), 0 };
+    return getFreeMemoryImpl("SystemTotalMemory", sums, sumsLen, 1);
 #endif
     return 0;
 }
