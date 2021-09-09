863927d6738fedfd0d365ad81ce85d2aa61f509c
Michael Steliaros <mike@steliaros.com>
2021-09-09 20:42:55 +0100

    Do not cause an allocation every frame; cache Application.persistentDataPath

--- Assets/MemoryTest.cs
+++ Assets/MemoryTest.cs
@@ -5,51 +5,52 @@ using UnityEngine.UI;
 [ExecuteInEditMode]
 public class MemoryTest : MonoBehaviour
 {
-	public Text text;
+    public Text text;
 
-	private StringBuilder _builder = new StringBuilder();
-	private const long BYTE_TO_MEGABYTE = 1000 * 1000;
+    private string _path;
+    private readonly StringBuilder _builder = new StringBuilder();
+    private const long BYTE_TO_MEGABYTE = 1000 * 1000;
 
-	private void OnEnable()
-	{
-		if (null == text)
-		{
-			enabled = false;
-			return;
-		}
-	}
+    private void OnEnable()
+    {
+        _path = Application.persistentDataPath;
 
-	private void Update()
-	{
-		_builder.Length = 0;
+        if (null == text)
+        {
+            enabled = false;
+            return;
+        }
+    }
 
-		{
-			long memory = Memory.processResidentUsed;
-			_builder.Append("Process Resident: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+    private void Update()
+    {
+        _builder.Length = 0;
 
-			memory = Memory.processVirtualUsed;
-			_builder.Append("Process Virtual: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+        {
+            long memory = Memory.processResidentUsed;
+            _builder.Append("Process Resident: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
 
-			memory = Memory.systemFree;
-			_builder.Append("System Free: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+            memory = Memory.processVirtualUsed;
+            _builder.Append("Process Virtual: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
 
-			memory = Memory.systemTotal;
-			_builder.Append("System Total: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
-		}
+            memory = Memory.systemFree;
+            _builder.Append("System Free: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
 
-		{
-			var path = Application.persistentDataPath;
+            memory = Memory.systemTotal;
+            _builder.Append("System Total: ").Append(memory / BYTE_TO_MEGABYTE).Append('.').Append((memory % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+        }
 
-			var size = Memory.TotalSize(path);
-			_builder.Append("FS Total: ").Append(size / BYTE_TO_MEGABYTE).Append('.').Append((size % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+        {
+            var size = Memory.TotalSize(_path);
+            _builder.Append("FS Total: ").Append(size / BYTE_TO_MEGABYTE).Append('.').Append((size % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
 
-			size = Memory.FreeSize(path);
-			_builder.Append("FS Free: ").Append(size / BYTE_TO_MEGABYTE).Append('.').Append((size % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+            size = Memory.FreeSize(_path);
+            _builder.Append("FS Free: ").Append(size / BYTE_TO_MEGABYTE).Append('.').Append((size % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
 
-			size = Memory.AvailableSize(path);
-			_builder.Append("FS Available: ").Append(size / BYTE_TO_MEGABYTE).Append('.').Append((size % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
-		}
+            size = Memory.AvailableSize(_path);
+            _builder.Append("FS Available: ").Append(size / BYTE_TO_MEGABYTE).Append('.').Append((size % BYTE_TO_MEGABYTE) / (BYTE_TO_MEGABYTE / 10)).Append("MB").AppendLine();
+        }
 
-		text.text = _builder.ToString();
-	}
+        text.text = _builder.ToString();
+    }
 }
